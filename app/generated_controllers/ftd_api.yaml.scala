fa42ab5998d207187ea5d181a68015365cb87dde
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-06-22 15:49:24 +0200

    Fixed route bug and deal with INt in DashboardUtil

--- app/generated_controllers/ftd_api.yaml.scala
+++ app/generated_controllers/ftd_api.yaml.scala
@@ -31,7 +31,7 @@ import services.dashboard.DashboardRegistry
 
 package ftd_api.yaml {
     // ----- Start of unmanaged code area for package Ftd_apiYaml
-        
+                    
     // ----- End of unmanaged code area for package Ftd_apiYaml
     class Ftd_apiYaml @Inject() (
         // ----- Start of unmanaged code area for injections Ftd_apiYaml
