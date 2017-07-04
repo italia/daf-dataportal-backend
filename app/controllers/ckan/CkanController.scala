f1509cfc7ebc19c9114281cd7430d514d0f7ea13
acherici <a.cherici@gmail.com>
2017-07-04 19:16:31 +0200

    [DATPOR-26] Main CKAN APIs have been integrated

--- app/controllers/ckan/CkanController.scala
+++ app/controllers/ckan/CkanController.scala
@@ -13,7 +13,6 @@ import play.api.libs.ws._
 import scala.concurrent.Future
 import play.api.libs.json._
 import play.api.inject.ConfigurationProvider
-import services.ComponentRegistry
 import services.ckan.CkanRegistry
 
 
@@ -29,6 +28,8 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
   private val LOCAL_URL :String = config.get.getString("app.local.url").get
 
   private val ENV:String = config.get.getString("app.type").get
+
+  private val AUTH_TOKEN:String = config.get.getString("app.ckan.auth.token").get
 
 
   private def getOrgs(orgId :String): Future[List[String]] = {
@@ -63,7 +64,7 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
   def createDataset = Action.async { implicit request =>
 
     /*
-    curl -H "Content-Type: application/json" -X POST -d @data.json http://localhost:9000/ckan/createDataset  dove data json
+    curl -H "Content-Type: application/json" -X POST -d @data.json http://localhost:9000/ckan/createDataset  dove data.json
     contiene:
 
     {
@@ -155,7 +156,7 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
 
     */
 
-    val AUTH_TOKEN:String = config.get.getString("app.ckan.auth.token").get
+    //val AUTH_TOKEN:String = config.get.getString("app.ckan.auth.token").get
 
     val json:JsValue = request.body.asJson.get
 
@@ -173,6 +174,134 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
         println(x.json.toString)
         Ok(x.json)
       }
+    }
+  }
+
+
+  def updateDataset(datasetId :String)= Action.async { implicit request =>
+    /*
+    curl -H "Content-Type: application/json" -X PUT -d @datavv.json http://localhost:9000/ckan/updateDataset/id=81b643bd-007e-44cb-b724-0a02018db6d9
+    dove datavv.json contiene:
+  {
+      "license_title":"Creative Commons Attribution",
+      "maintainer":"",
+      "geographical_geonames_url":"",
+      "issued":"",
+      "private":false,
+      "maintainer_email":"",
+      "num_tags":1,
+      "frequency":"ANNUAL",
+      "publisher_name":"andrea",
+      "id":"81b643bd-007e-44cb-b724-0a02018db6d9",
+      "metadata_created":"2017-06-27T17:08:10.884624",
+      "creator_identifier":"",
+      "conforms_to":"",
+      "metadata_modified":"2017-06-27T17:15:06.150414",
+      "author":"",
+      "author_email":"",
+      "isopen":true,
+      "theme":"{SOCI,TRAN}",
+      "temporal_start":"",
+      "state":"active",
+      "version":"",
+      "alternate_identifier":"",
+      "relationships_as_object":[
+
+      ],
+      "license_id":"cc-by",
+      "type":"dataset",
+      "resources":[
+         {
+            "mimetype":null,
+            "cache_url":null,
+            "hash":"",
+            "description":"",
+            "name":"",
+            "format":"CSV",
+            "url":"http://localhost/pippo.csv",
+            "cache_last_updated":null,
+            "package_id":"81b643bd-007e-44cb-b724-0a02018db6d9",
+            "created":"2017-06-27T17:08:56.845480",
+            "state":"active",
+            "mimetype_inner":null,
+            "distribution_format":"CSV",
+            "last_modified":null,
+            "position":0,
+            "revision_id":"d679dd2b-b972-4f07-b0f4-fed58aea646a",
+            "url_type":null,
+            "id":"558299e5-cb15-4f6d-ae0c-9beb6f83f949",
+            "resource_type":null,
+            "size":null
+         }
+      ],
+      "holder_name":"andrea",
+      "tags":[
+         {
+            "vocabulary_id":null,
+            "state":"active",
+            "display_name":"economy",
+            "id":"545104f0-9d0d-46bb-94cd-0ebd0da204d3",
+            "name":"economy"
+         }
+      ],
+      "holder_identifier":"231224123234",
+      "groups":[
+
+      ],
+      "creator_user_id":"c7bdf748-7798-41e7-87e6-0be3b5ede2ad",
+      "temporal_end":"",
+      "relationships_as_subject":[
+
+      ],
+      "is_version_of":"",
+      "num_resources":1,
+      "name":"mydataset",
+      "creator_name":"",
+      "url":"",
+      "notes":"prova",
+      "owner_org":"99154d6b-af89-4797-beb1-14e2a6823672",
+      "modified":"2017-06-15",
+      "publisher_identifier":"test",
+      "geographical_name":"ITA_AGR",
+      "license_url":"http://www.opendefinition.org/licenses/cc-by",
+      "title":"dataset di prova",
+      "revision_id":"6d70740c-c50c-412a-8705-8c5ff659e96b",
+      "identifier":"myDatasetId"
+   }
+    */
+
+
+    val json:JsValue = request.body.asJson.get
+
+    val response = ws.url(CKAN_URL + "/api/3/action/package_update?id=" + datasetId).withHeaders("Authorization" -> AUTH_TOKEN).post(json)
+
+    response map { x =>
+      Ok(x.json)
+    }
+
+  }
+
+  def deleteDataset(datasetId :String) = Action.async { implicit request =>
+
+    //curl -H "Content-Type: application/json" -X DELETE http://localhost:9000/ckan/deleteDataset/49f3d5f9-5ad6-4451-9bca-b3287185d19d
+
+    val url = CKAN_URL + "/api/3/action/package_delete"
+    val body = s"""{\"id\":\"$datasetId\"}"""
+    val test = ws.url(url).withHeaders("Authorization" -> AUTH_TOKEN).post(body)
+    test map { response =>
+      Ok(response.json)
+    }
+  }
+
+  def purgeDataset(datasetId :String) = Action.async { implicit request =>
+
+    //curl -H "Content-Type: application/json" -X DELETE http://localhost:9000/ckan/purgeDataset/mydataset
+
+    val url = CKAN_URL + "/api/3/action/dataset_purge"
+    val body = s"""{\"id\":\"$datasetId\"}"""
+    val test = ws.url(url).withHeaders("Authorization" -> AUTH_TOKEN).post(body)
+    test map { response =>
+      Ok(response.json)
     }
   }
 
@@ -195,7 +324,7 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
     }
     * */
 
-    val AUTH_TOKEN:String = config.get.getString("app.ckan.auth.token").get
+    //val AUTH_TOKEN:String = config.get.getString("app.ckan.auth.token").get
     val json:JsValue = request.body.asJson.get
 
     val response = ws.url(CKAN_URL + "/api/3/action/organization_create").withHeaders("Authorization" -> AUTH_TOKEN).post(json)
@@ -207,9 +336,65 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
 
   }
 
-  // added
+  def updateOrganization(orgId :String)= Action.async { implicit request =>
+
+    // curl -H "Content-Type: application/json" -X PUT -d @org.json http://localhost:9000/ckan/updateOrganization/id=232cad97-ecf2-447d-9656-63899023887t
+
+    val json:JsValue = request.body.asJson.get
+
+    val response = ws.url(CKAN_URL + "/api/3/action/organization_update?id=" + orgId).withHeaders("Authorization" -> AUTH_TOKEN).post(json)
+
+    response map { x =>
+      Ok(x.json)
+    }
+
+  }
+
+
+  def deleteOrganization(orgId :String) = Action.async { implicit request =>
+
+
+    //curl -H "Content-Type: application/json" -X DELETE http://localhost:9000/ckan/deleteOrganization/apt-altopiano-di-pine-e-valle-di-cembra2
+
+    val url = CKAN_URL + "/api/3/action/organization_delete"
+    val body = s"""{\"id\":\"$orgId\"}"""
+
+    val test = ws.url(url).withHeaders("Authorization" -> AUTH_TOKEN).post(body)
+    test map { response =>
+      Ok(response.json)
+    }
+  }
+
+
+  def purgeOrganization(orgId :String) = Action.async { implicit request =>
+
+    //curl -H "Content-Type: application/json" -X DELETE http://localhost:9000/ckan/purgeOrganization/apt-altopiano-di-pine-e-valle-di-cembra2
+
+    val url = CKAN_URL + "/api/3/action/organization_purge"
+    val body = s"""{\"id\":\"$orgId\"}"""
+
+    val test = ws.url(url).withHeaders("Authorization" -> AUTH_TOKEN).post(body)
+    test map { response =>
+      Ok(response.json)
+    }
+  }
+
+
   def getDatasetList = Action.async { implicit request =>
     val test = ws.url(CKAN_URL + "/api/3/action/package_list").get
+    test map { response =>
+      Ok(response.json)
+    }
+  }
+
+
+  def getDatasetListWithResources(limit:Int, offset:Int) = Action.async { implicit request =>
+
+    // curl -X GET "http://localhost:9000/ckan/datasetsWithResources?limit=1&offset=1"
+
+    val url = CKAN_URL + s"/api/3/action/current_package_list_with_resources?limit=$limit&offset=$offset"
+    //println("url-->"+url)
+    val test = ws.url(url).get
     test map { response =>
       Ok(response.json)
     }
@@ -224,7 +409,7 @@ class CkanController @Inject() (ws: WSClient, config: ConfigurationProvider) ext
     }
   }
 
-  //added
+
   def searchDataset(q :String) = Action.async { implicit request =>
 
     val url = CKAN_URL + "/api/3/action/package_search?q=" + q
