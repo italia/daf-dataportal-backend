3a29a69d1dc673f563fc0e5e7853556aebf48060
Alessandro Ercolani <ale.ercolani@gmail.com>
2017-12-06 13:29:15 +0100

    Bored zalando

--- app/generated_controllers/ftd_api.yaml.scala
+++ app/generated_controllers/ftd_api.yaml.scala
@@ -54,7 +54,7 @@ import services.settings.SettingsRegistry
 
 package ftd_api.yaml {
     // ----- Start of unmanaged code area for package Ftd_apiYaml
-    
+        
     // ----- End of unmanaged code area for package Ftd_apiYaml
     class Ftd_apiYaml @Inject() (
         // ----- Start of unmanaged code area for injections Ftd_apiYaml
