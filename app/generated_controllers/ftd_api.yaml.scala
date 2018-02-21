1c609f570d92c415268b96d97c0fc3a3412463ab
luca-pic <luca.xxvi@gmail.com>
2018-02-21 13:59:20 +0100

    change UserStory

--- app/generated_controllers/ftd_api.yaml.scala
+++ app/generated_controllers/ftd_api.yaml.scala
@@ -19,6 +19,16 @@ import javax.inject._
 
 import java.io.File
 
+import play.api.mvc.{Action,Controller}
+import play.api.data.validation.Constraint
+import play.api.i18n.MessagesApi
+import play.api.inject.{ApplicationLifecycle,ConfigurationProvider}
+import de.zalando.play.controllers._
+import PlayBodyParsing._
+import PlayValidations._
+import scala.util._
+import javax.inject._
+import java.io.File
 import de.zalando.play.controllers.PlayBodyParsing._
 import it.gov.daf.common.authentication.Authentication
 import org.pac4j.play.store.PlaySessionStore
@@ -47,7 +57,7 @@ import it.gov.daf.common.authentication.Role
 
 package ftd_api.yaml {
     // ----- Start of unmanaged code area for package Ftd_apiYaml
-                            
+        
     // ----- End of unmanaged code area for package Ftd_apiYaml
     class Ftd_apiYaml @Inject() (
         // ----- Start of unmanaged code area for injections Ftd_apiYaml
@@ -176,6 +186,7 @@ package ftd_api.yaml {
         val inferschema = inferschemaAction { input: (File, String) =>
             val (upfile, fileType) = input
             // ----- Start of unmanaged code area for action  Ftd_apiYaml.inferschema
+            // DEPRECATED NOT USED ANYMORE WE ARE USING KYLO
             if (fileType.equals("csv")) {
 
         val (content, rows) = Source
