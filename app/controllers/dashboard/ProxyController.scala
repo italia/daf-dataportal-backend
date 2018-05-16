9b0d88c78f05243290179cdaa40d6d7864347820
Alessandro Ercolani <ale.ercolani@gmail.com>
2018-05-16 11:30:59 +0200

    Added public api for dataset

--- app/controllers/dashboard/ProxyController.scala
+++ app/controllers/dashboard/ProxyController.scala
@@ -2,10 +2,15 @@ package controllers.dashboard
 
 import javax.inject.Inject
 
+import com.mongodb.casbah.Imports.{MongoCredential, MongoDBObject, ServerAddress}
+import com.mongodb.casbah.MongoClient
+import ftd_api.yaml.Success
 import play.api.cache.CacheApi
 import play.api.inject.ConfigurationProvider
+import play.api.libs.json.Json
 import play.api.libs.ws.{WSClient, WSResponse}
 import play.api.mvc.{Action, Controller}
+import utils.ConfigReader
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,6 +19,15 @@ class ProxyController @Inject()(ws: WSClient,
                                 config: ConfigurationProvider
                                 )(implicit context: ExecutionContext) extends Controller {
 
+  private val userName = ConfigReader.userName
+  private val source = ConfigReader.database
+  private val password = ConfigReader.password
+
+  private val mongoHost: String = ConfigReader.getDbHost
+  private val mongoPort = ConfigReader.getDbPort
+
+  val server = new ServerAddress(mongoHost, 27017)
+  val credentials = MongoCredential.createCredential(userName, source, password.toCharArray)
 
   def medium(url: String) = Action.async { implicit request =>
     val scheme = url.matches("^http[s]?://.*") match {
@@ -49,4 +63,33 @@ class ProxyController @Inject()(ws: WSClient,
       Ok(response.body).as("application/json")
     }
   }
+
+  def externalOpendata(name :String) = Action {  request =>
+    val mongoClient = MongoClient(server, List(credentials))
+    val db = mongoClient(source)
+    val coll = db("ext_opendata")
+    val query = MongoDBObject("name" -> name)
+    val dataset = coll.findOne(query)
+    val json = dataset match {
+      case None => Json.parse("{}")
+      case Some(ser) =>  {
+        val jsonString = com.mongodb.util.JSON.serialize(dataset)
+        Json.parse(jsonString)
+      }
+    }
+    Ok(json)
+  }
+
+  def dafResourcesInExtOpendata(name :String) = Action {  request =>
+    val mongoClient = MongoClient(server, List(credentials))
+    val db = mongoClient(source)
+    val coll = db("catalog_test")
+    val query = MongoDBObject("operational.ext_opendata.name" -> name)
+    val datasets = coll.find(query).toList
+    val jsonString = com.mongodb.util.JSON.serialize(datasets)
+    val json = Json.parse(jsonString)
+    Ok(json)
+  }
+
+
 }
